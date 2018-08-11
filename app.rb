097549242dc01e7e43fa3bbde3cb19f765b022d7
Brent Bauman <brentbauman86@gmail.com>
2018-08-11 16:21:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,10 +7,10 @@ class App < Sinatra::Base
   end
 
   get '/hometown' do
-    "My hometown is Berne, IN not Berne, Switzerland"
+    "My hometown is Berne, IN not Bern, Switzerland"
   end
 
   get '/favorite-song' do
-    "My favorite song is Sons and Daughters"
+    "My favorite song is 'Sons and Daughters'"
   end
 end
